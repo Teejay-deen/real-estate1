9eb71b571cbc7d97b6807d09a79b6b2b77fb5a13
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-03-12 01:51:38 +0100

    added the houses components

--- src/components/House.js
+++ src/components/House.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from "react";
+import { BiBed, BiBath, BiArea } from "react-icons/bi";
 
-const House = () => {
-  return <div>House</div>;
+const House = ({ house }) => {
+  const { image, type, address, country, bedrooms, bathrooms, surface, price } =
+    house;
+  return (
+    <div className="bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition">
+      <img className="mb-8 " src={image} alt="" />
+      <div className="mb-4 gap-x-2 flex text-sm">
+
+        <div className="bg-green-700 rounded-full text-white px-4">{type}</div>
+
+        <div className="bg-violet-500 rounded-full text-white px-4">
+          {country}
+        </div>
+      </div>
+      <div className="text-lg max-w-[260px] font-semibold ">{address}</div>
+      <div className="flex gap-x-4 my-4">
+        <div className="flex gap-1 text-gray-600 items-center">
+          <div className="text-[20px]">
+            <BiBed />
+          </div>
+          <div>{bedrooms}</div>
+        </div>
+
+        <div className="flex gap-1 text-gray-600 items-center">
+          <div className="text-[20px]">
+            <BiBath />
+          </div>
+          <div>{bathrooms}</div>
+        </div>
+
+        <div className="flex gap-1 text-gray-600 items-center">
+          <div className="text-[20px]">
+            <BiArea />
+          </div>
+          <div>{surface}</div>
+        </div>
+      </div>
+      <div className="text-violet-600 font-semibold mb-4 ">{price}</div>
+    </div>
+  );
 };
 
 export default House;
--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+//import context
+import { HouseContext } from "./HouseContext";
+//import component 
+import House from "./House"
+//import link
+import { Link } from "react-router-dom";
+//import icons
+import { ImSpinner2} from "react-icons/im"
 
 const HouseList = () => {
-  return <div>HouseList</div>;
+  const { houses,  loading } = useContext(HouseContext)
+  console.log(houses);
+  return <section className="mb-20 ">
+    <div className="container mx-auto">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14">
+        {houses.map((house, index)=>{
+          return(
+            <Link to={`/property/${houses.id}`} key={index}>
+              <House house ={house} />
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  </section>;
 };
 
 export default HouseList;
