7498a97a820316eded6368c6d2c96359bf8c059e
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-08-30 15:46:40 +0100

    fixes error

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ProprtyDetails from "./pages/PropertyDetails";
 
+
 const App = () => {
     return ( <
         div className = "max-w-[1440px] mx-auto bg-white" >
