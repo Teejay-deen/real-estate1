e34dc81c749fe32505b8a6b95df755a70a551df1
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-08-30 15:45:10 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ import Home from "./pages/Home";
 import ProprtyDetails from "./pages/PropertyDetails";
 
 const App = () => {
-  return (
-    <div className="max-w-[1440px] mx-auto bg-white">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/property/:id" element={<ProprtyDetails />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return ( <
+        div className = "max-w-[1440px] mx-auto bg-white" >
+        <
+        Header / >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/property/:id"
+        element = { < ProprtyDetails / > }
+        /> <
+        /Routes> <
+        Footer / >
+        <
+        /div>
+
+    );
 };
 
 export default App;
